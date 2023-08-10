02a6c690e2dd48f65da205987192748e6631628c
Muhammad Ahmed <ahmedkhan871871@gmail.com>
2023-08-10 09:11:26 +0500

    Auto Dispose

--- lib/providers/posts_provider/post_data_provider.dart
+++ lib/providers/posts_provider/post_data_provider.dart
@@ -4,3 +4,7 @@ import 'package:riverpod_api_demo/providers/posts_provider/posts_state.dart';
 
 final postsProvider =
     StateNotifierProvider<PostsNotifier, PostsState>((ref) => PostsNotifier());
+
+/// If you don't want to preserve the state add (autoDispose) along with State Notifier Provider
+// final postsProvider =
+// StateNotifierProvider.autoDispose<PostsNotifier, PostsState>((ref) => PostsNotifier());
